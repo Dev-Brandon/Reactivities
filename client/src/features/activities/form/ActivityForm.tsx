62b1fe5f04828657c810e4055502e750038eb276
Dev-Brandon <contact@devbrandon.com>
2019-09-14 22:03:01 -0400

    Added State management with mobx

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,22 +1,16 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useContext } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from '../../../app/models/activity';
 import {v4 as uuid} from 'uuid';
-
+import ActivityStore from '../../../app/stores/activityStore';
+import { observer } from "mobx-react-lite";
 interface IProps {
-  setEditMode: (editMode: boolean) => void;
   activity: IActivity;
-  createActivity: (activity: IActivity) => void;
-  editActivity: (activity: IActivity) => void;
-  submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
-  setEditMode, 
-  activity: initialFormState,
-   createActivity, 
-   editActivity,
-   submitting}) => {
+  activity: initialFormState
+   }) => {
 
 const initializeForm = ()  => {
   if (initialFormState) {
@@ -52,7 +46,8 @@ const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaEleme
   const {name, value} = event.currentTarget;
   setActivity({...activity, [name]: value})
 }
-
+  const activityStore = useContext(ActivityStore)
+  const {createActivity, editActivity, submitting, cancelFormOpen} = activityStore;
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit}>
@@ -95,10 +90,10 @@ const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaEleme
         value={activity.venue} 
         />
         <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
-        <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel' />
+        <Button onClick={() => cancelFormOpen} floated='right' type='button' content='Cancel' />
       </Form>
     </Segment>
   );
 };
 
-export default ActivityForm;
+export default observer(ActivityForm);
